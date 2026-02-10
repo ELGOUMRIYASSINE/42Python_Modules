8c64044704ae55c66483ba825ec08b005e43ec0a
Yassine <yassinegomri2004@gmail.com>
2026-02-10 20:01:18 +0100

    complete exerice 1 module 05 python

--- Python_Module_05/ex0/stream_processor.py
+++ Python_Module_05/ex0/stream_processor.py
@@ -3,86 +3,118 @@ from typing import Any
 
 
 class DataProcessor(ABC):
-    """Abstract base class for data processors"""
-
     @abstractmethod
     def process(self, data: Any) -> str:
-        """Process the data and return result string"""
         pass
 
     @abstractmethod
     def validate(self, data: Any) -> bool:
-        """Validate if data is appropriate for this processor"""
         pass
 
     def format_output(self, result: str) -> str:
-        """Default formatting - can be overridden in subclasses"""
-        pass
+        return f"Output : {result}"
 
 
 class NumericProcessor(DataProcessor):
-    """Processor for numeric data"""
 
     def process(self, data: Any) -> str:
-        """Process numeric data"""
-        pass
+        length = 0
+        avg = 0
+        sum = 0
+        if isinstance(data, int):
+            length = 1
+            sum = data
+            avg = sum / length
+        else:
+            for nb in data:
+                length += 1
+                sum += nb
+            avg = sum / length
+        return (f"Processed {length} numeric values, sum={sum},"
+                f" avg={avg:.1f}")
 
     def validate(self, data: Any) -> bool:
-        """Validate numeric data"""
-        for value in data:
-            if not isinstance(value, int):
-                return False
+        if isinstance(data, (int, float)):
+            return True
+        else:
+            for value in data:
+                if not isinstance(value, (int, float)):
+                    return False
         return True
-                
-
-    def format_output(self, result: str) -> str:
-        """Format numeric output"""
-        pass
 
 
 class TextProcessor(DataProcessor):
-    """Processor for text data"""
 
     def process(self, data: Any) -> str:
-        """Process text data"""
-        pass
+        length = data.__len__()
+        words = data.split().__len__()
+        return f"Processed text: {length} characters, {words} words"
 
     def validate(self, data: Any) -> bool:
-        """Validate text data"""
         if not isinstance(data, str) or not data:
             return False
         return True
 
-    def format_output(self, result: str) -> str:
-        """Format text output"""
-        pass
-
 
 class LogProcessor(DataProcessor):
-    """Processor for log data"""
 
     def process(self, data: Any) -> str:
-        """Process log data"""
-        pass
+        if "ERROR" in data:
+            return f"[ALERT] ERROR level detected:{data.split(':')[1]}"
+        if "INFO" in data:
+            return f"[INFO] INFO level detected:{data.split(':')[1]}"
 
     def validate(self, data: Any) -> bool:
-        """Validate log data"""
         if not isinstance(data, str):
             return False
-        if not "[" in data or not "]" in data:
+        if "ERROR" not in data and "INFO" not in data:
+            return False
+        if ":" not in data:
             return False
         return True
-
-    def format_output(self, result: str) -> str:
-        """Format log output"""
-        pass
 
 
 def process_data(processor: DataProcessor, data: Any) -> None:
     """Polymorphic function that works with any DataProcessor"""
-    if processor.validate():
+    print(f"Processing data: {data}")
+    try:
+        if processor.validate(data):
+            if isinstance(processor, NumericProcessor):
+                print("Validation: Numeric data verified")
+            if isinstance(processor, TextProcessor):
+                print("Validation: Text data verified")
+            if isinstance(processor, LogProcessor):
+                print("Validation: Log entry verified")
+            result = processor.process(data)
+            print(processor.format_output(result))
+        else:
+            print("Error: Invalid Data")
+    except Exception as e:
+        print(f"Error: somthing wrong {e}")
 
 
-# Main execution
 if __name__ == "__main__":
-    pass
+    print("=== CODE NEXUS - DATA PROCESSOR FOUNDATION ===\n")
+    print("Initializing Numeric Processor...")
+    process_data(NumericProcessor(), 4)
+
+    print()
+    print("Initializing Text Processor...")
+    process_data(TextProcessor(), "Hello Nexus World")
+
+    print()
+    print("Initializing Log Processor...")
+    process_data(LogProcessor(), "ERROR: Connection timeout")
+
+    print("\n=== Polymorphic Processing Demo ===")
+    print("Processing multiple data types through same interface...")
+    processes = [NumericProcessor(), TextProcessor(), LogProcessor()]
+    data = [4, "My name is yassine", "INFO: System ready"]
+    i = 0
+    for process in processes:
+        if process.validate(data[i]):
+            print(f"Result {i + 1}: {process.process(data[i])}")
+        else:
+            print(f"Error: Invalide Data => {data[i]}")
+        i += 1
+    print("\nFoundation systems online. Nexus ready for advanced streams.")
