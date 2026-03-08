ca1ca3c4149e196587d2951cfc8ce521521bf27e
Yassine <yassinegomri2004@gmail.com>
2026-03-08 01:59:29 +0000

    complete first exercice module 10

---
+++ Python_Module_10/ex0/lambda_spells.py
@@ -0,0 +1,48 @@
+mages = [
+    {"name": "Aelric", "power": 120, "type": "fire"},
+    {"name": "Lyra", "power": 95, "type": "ice"},
+    {"name": "Thalion", "power": 150, "type": "arcane"},
+    {"name": "Seraphina", "power": 110, "type": "lightning"},
+    {"name": "Darius", "power": 80, "type": "shadow"},
+    {"name": "Elowen", "power": 70, "type": "nature"},
+    {"name": "Kael", "power": 130, "type": "fire"}
+]
+
+spells = [
+    "fireball",
+    "teleport",
+    "invisibility"
+]
+
+
+def artifact_sorter(artifacts: list[dict]) -> list[dict]:
+    return sorted(artifacts, key=lambda artifacts: artifacts["power"], reverse=True)
+
+
+def power_filter(mages, min_power):
+    return list(filter(lambda mage : mage['power'] >= min_power, mages))
+
+
+def spell_transformer(spells):
+    return list(map(lambda spell: "* " + spell + " *", spells))
+
+
+def mage_stats(mages):
+    result = {"max_power": None, "min_power": None, "avg_power": None}
+    result["max_power"] = max(map(lambda mage: mage['power'], mages))
+    result["min_power"] = min(map(lambda mage: mage['power'], mages))
+    result["avg_power"] = sum(map(lambda mage: mage['power'], mages)) / len(mages)
+
+    return result
+
+print("Testing artifact sorter...")
+print(artifact_sorter(mages))
+print("")
+print("Testing power filter...")
+print(power_filter(mages, 100))
+print("")
+print("Testing spell transformer...")
+print(spell_transformer(spells))
+print("")
+print("Testing mage stats...")
+print(mage_stats(mages))
--- Python_Module_10/test.py
+++ Python_Module_10/test.py
@@ -1,8 +1,11 @@
 numbers = [1, 2, 3, 4, 5]
 
+students = [("Emil", 25), ("Tobias", 22), ("Linus", 28)]
 
 def multi_2(number):
     return number * 2
 
-doubles = list(map(multi_2, numbers))
+# doubles = list(map(lambda x: x * 2, numbers))
+# doubles = list(filter(lambda x: x % 2 != 0, numbers))
+doubles = list(sorted(students, key=lambda x: x[1]))
 print(doubles)
